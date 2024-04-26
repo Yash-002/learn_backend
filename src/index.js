ad481c93b2da2080f81b0f748d6b037fe897b262
Yash Maurya <yashmaurya.in@gmail.com>
2024-04-26 20:39:35 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import { DBconnect } from "./DB/DBconnect.js";
+dotenv.config({ path: ".env" });
+
+DBconnect()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(
+                `🏃🏼‍♀️‍➡️ Server is running on http://localhost:${process.env.PORT}`
+            );
+        });
+    })
+    .catch((error) => {
+        console.log("🔴 Error happend :", error.message);
+    });
